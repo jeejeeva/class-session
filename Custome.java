0aceedc3d926389c301162597c2b00a179345c32
jeeva <jeejeeva>
2022-10-11 14:28:51 +0530

    Ticket book program

---
+++ Custome.java
@@ -0,0 +1,10 @@
+class Custome
+{
+public static void main(String[] args)
+{
+    
+    Theat theatre = new Theat();
+    theatre.book_ticket(200);//     book_ticket
+
+}
+}
---
+++ Theat.java
@@ -0,0 +1,7 @@
+class Theat 
+{
+void book_ticket(int amount)
+{
+    System.out.println("Balance = "+(amount-120));
+}
+}
